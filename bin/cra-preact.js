f17c45639070f8a661595337e4c9702a75dec79b
shilangyu <xmarcinmarcin@gmail.com>
2020-03-02 12:46:07 +0100

    testing for bad scripts

--- bin/cra-preact.js
+++ bin/cra-preact.js
@@ -13,7 +13,7 @@ if (args.length === 1 && ['build', 'start', 'test'].includes(args[0])) {
 	try {
 		require(`cra-preact/src/${action}`)
 	} catch (e) {
-		exitWith('Failed to run the script. Are you sure you have react-scripts v3+?\n', e)
+		exitWith(`Failed to run the '${action}' script. Are you sure you have react-scripts v3+?\n${e}`)
 	}
 } else if (args.includes('eject')) {
 	exitWith(`Don't eject with cra-preact, it does nothing special. Eject with react-scripts.
